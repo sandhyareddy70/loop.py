5c33c84865eab9cb6870dcb7cb2aaa365dc0320d
sandhyareddy70 <140163218+sandhyareddy70@users.noreply.github.com>
2024-01-02 15:27:02 +0530

    Add files via upload

---
+++ swaping  of two numbers.py
@@ -0,0 +1,39 @@
+# a=int(input("Enter a number a:"))
+# b=int(input("Enter a number b:"))
+# a,b=b,a
+# print("a:",a)
+# print("b:",b)
+
+# a=int(input("Enter a number a:"))
+# b=int(input("Enter a number b:"))
+# c=a
+# a=b
+# b=c
+# print("a:",a)
+# print("b:",b)
+
+# a=int(input("Enter a number a:"))
+# b=int(input("Enter a number b:"))
+# a=a+b
+# b=a-b
+# a=a-b
+# print("a:",a)
+# print("b:",b)
+
+# a=int(input("Enter a number a:"))
+# b=int(input("Enter a number b:"))
+# a=a*b
+# b=a//b
+# a=a//b
+# print("a:",a)
+# print("b:",b)
+
+#XOR Swap
+# a=int(input("Enter a number a:"))
+# b=int(input("Enter a number b:"))
+# a=a^b
+# b=a^b
+# a=a^b
+# print("a:",a)
+# print("b:",b)
+
